b79c05b0b1b88ed8c9636ed42eb216b7bcb05f0d
Shivani Kakrecha <shivanikakrecha@gmail.com>
2024-06-06 19:37:27 +0530

    Updates

---
+++ precache-manifest.c50b81b23729e319fe5fac05a23978a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69b41af4787329ae95cddfdf44da64c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac51f91dc2c518c02e43",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "36696d151e3f8e126348",
+    "url": "/static/js/2.de4b792b.chunk.js"
+  },
+  {
+    "revision": "c22e7b4cb20d4556252c2aa78698d764",
+    "url": "/static/js/2.de4b792b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c50c414c7351dd94669",
+    "url": "/static/js/3.d9aeb76b.chunk.js"
+  },
+  {
+    "revision": "ac51f91dc2c518c02e43",
+    "url": "/static/js/main.beb07785.chunk.js"
+  },
+  {
+    "revision": "116ccbcc79dc07ebb89e",
+    "url": "/static/js/runtime-main.02be03c1.js"
+  }
+]);
