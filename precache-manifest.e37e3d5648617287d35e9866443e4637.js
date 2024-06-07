78744dc29bb2b182dd74f7a580bdc740b59a9b5a
Shivani Kakrecha <shivanikakrecha@gmail.com>
2024-06-08 01:22:57 +0530

    Updates

---
+++ precache-manifest.e37e3d5648617287d35e9866443e4637.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7979c8895e57d05b5363b9acb31d9ab",
+    "url": "/vocab-frontend/index.html"
+  },
+  {
+    "revision": "46433a1da9b8b0c81799",
+    "url": "/vocab-frontend/static/js/2.4ce95c49.chunk.js"
+  },
+  {
+    "revision": "e0aeb2e98cdd84f2adce32d4a84e83da",
+    "url": "/vocab-frontend/static/js/2.4ce95c49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4764866ef2f35d89dbf2",
+    "url": "/vocab-frontend/static/js/3.5bf2edd1.chunk.js"
+  },
+  {
+    "revision": "0b9e3c028b82e69488db",
+    "url": "/vocab-frontend/static/js/main.7b7d9b0e.chunk.js"
+  },
+  {
+    "revision": "c953eebbd384e8d89fa2",
+    "url": "/vocab-frontend/static/js/runtime-main.cb4707c9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vocab-frontend/precache-manifest.b97122278fc0e7e59dd5247657902f14.js"
+  "/vocab-frontend/precache-manifest.e37e3d5648617287d35e9866443e4637.js"
 );
 
 self.addEventListener('message', (event) => {
